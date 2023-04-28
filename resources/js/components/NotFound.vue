60fbcd0cb098bba784dfd1303696a2419f3e2385
tajweb <tajweb.m@gmail.com>
2023-04-28 16:54:57 +0500

    feat: added vue

---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Not Found</h1>
+    <br>
+    <router-link to="/">Home page</router-link>
+</template>
+
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/invoices/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Index invoice page component</h1>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
